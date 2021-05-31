7de3502465cde74eec012cb065ae25a0477ad3b3
Nextttttt <dormanzhiev@gmail.com>
2021-05-31 06:47:47 +0300

    Updating Styles

---
+++ UniversitySystem/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿namespace UniversitySystem.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using UniversitySystem.Models.Comunication.Posts;
+    using UniversitySystem.Services.Interfaces;
+
+    public class HomeController : Controller
+    {
+        private readonly IPostsService postsService;
+
+        public HomeController(IPostsService postsService)
+        {
+            this.postsService = postsService;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<PostModel> posts = this.postsService.GetAll();
+
+            return View(posts);
+        }
+    }
+}
---
+++ UniversitySystem/Data/Migrations/20210531031324_Final.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversitySystem.Data.Migrations
+{
+    public partial class Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.CreateTable(
+                name: "PostBindingModel",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostBindingModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostUpdateBindingModel",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostUpdateBindingModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostViewModel",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostViewModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostBindingModel");
+
+            migrationBuilder.DropTable(
+                name: "PostUpdateBindingModel");
+
+            migrationBuilder.DropTable(
+                name: "PostViewModel");
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                });
+        }
+    }
+}
